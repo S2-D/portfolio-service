e23a41df87aa4513ef454faeea4e52d2156ea7eb
sodam <sodam@LAPTOP-MTOF3AII.localdomain>
2021-02-27 02:36:21 +0900

    포트폴리오 작성하기 버튼 테스트

--- web/src/components/portfolio/edu.js
+++ web/src/components/portfolio/edu.js
@@ -17,14 +17,15 @@ const schema = yup.object().shape({
     .number()
     .integer(),
   // .required()
-  user_id : yup
+  user_id: yup
     .number()
   //   // .number()
   //   // .integer()
 });
 
 function Edu() {
-  // let history = useHistory();
+  let [form, setForm] = useState(false);
+
   const post = (data) => {
     axios.post(`http://localhost:5000/portfolio/edu`, data)
       .then(response => {
@@ -33,41 +34,52 @@ function Edu() {
         console.log("fail")
       })
   }
+
   return (
-    <Formik
-      validationSchema={schema}
-      onSubmit={(values) => {
-        values.user_id = window.user_id
-        console.log("values", values);
-        alert(JSON.stringify(values, null, 2));
-        post(values);
-      }}
-      initialValues={{
-        edu_sc_nm: '',
-        edu_major: '',
-        edu_gd_ck: 1,
-        user_id: ''
-      }}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        values,
-      }) => (
-        <Form noValidate onSubmit={handleSubmit}>
-          user_id :{window.user_id}
-          <h3>학력</h3>
+    <div>
+      <h3>학력</h3>
+      <Button onClick={()=>{setForm(!form)}}>
+        {
+          form == true
+          ? '닫기'
+          : '작성하기'
+        }
+      </Button>
+      {
+        form == true
+        ?<Formik
+        validationSchema={schema}
+        onSubmit={(values) => {
+          values.user_id = window.user_id
+          console.log("values", values);
+          alert(JSON.stringify(values, null, 2));
+          post(values);
+        }}
+        initialValues={{
+          edu_sc_nm: '',
+          edu_major: '',
+          edu_gd_ck: 1,
+          user_id: ''
+        }}
+      >
+        {({
+          handleSubmit,
+          handleChange,
+          values,
+        }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            user_id :{window.user_id}
             {/* 학교이름*/}
             <Form.Group>
               <Form.Label >학교이름</Form.Label>
-                <InputGroup hasValidation>
-                  <Form.Control
-                    type="text"
-                    name="edu_sc_nm"
-                    value={values.edu_sc_nm}
-                    onChange={handleChange}
-                  />
-                </InputGroup>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type="text"
+                  name="edu_sc_nm"
+                  value={values.edu_sc_nm}
+                  onChange={handleChange}
+                />
+              </InputGroup>
               <ErrorMessage name="edu_sc_nm" component="p" />
             </Form.Group>
             {/* 전공  */}
@@ -85,42 +97,46 @@ function Edu() {
             {/* 상태 */}
             <fieldset>
               <Form.Group as={Row}>
-                  <Form.Check
-                    type="radio"
-                    inline
-                    label="재학중"
-                    value="1"
-                    name="formHorizontalRadios"
-                  />
-                  <Form.Check
-                    type="radio"
-                    inline
-                    label="학사졸업"
-                    value="2"
-                    name="formHorizontalRadios"
-                  />
-                  <Form.Check
-                    type="radio"
-                    inline
-                    label="석사졸업"
-                    value="3"
-                    name="formHorizontalRadios"
-                  />
-                  <Form.Check
-                    type="radio"
-                    inline
-                    label="박사졸업"
-                    value="4"
-                    name="formHorizontalRadios"
-                  />
+                <Form.Check
+                  type="radio"
+                  inline
+                  label="재학중"
+                  value="1"
+                  name="formHorizontalRadios"
+                />
+                <Form.Check
+                  type="radio"
+                  inline
+                  label="학사졸업"
+                  value="2"
+                  name="formHorizontalRadios"
+                />
+                <Form.Check
+                  type="radio"
+                  inline
+                  label="석사졸업"
+                  value="3"
+                  name="formHorizontalRadios"
+                />
+                <Form.Check
+                  type="radio"
+                  inline
+                  label="박사졸업"
+                  value="4"
+                  name="formHorizontalRadios"
+                />
                 <ErrorMessage name="edu_gd_ck" component="p" />
               </Form.Group>
             </fieldset>
-          <Button inline type="submit">확인</Button>
-          <Button inline >취소</Button>
-        </Form>
-      )}
-    </Formik>
+            <Button inline type="submit">확인</Button>
+            <Button inline >취소</Button>
+          </Form>
+        )}
+      </Formik>
+        : null
+      }
+      
+    </div>
   );
 }
 
